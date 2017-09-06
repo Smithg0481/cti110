aa3622ee7709a2f7d0edca994561cb5aa147a697
Smithg0481 <31482269+Smithg0481@users.noreply.github.com>
2017-09-06 11:13:35 -0400

    sales perdection

---
+++ M2T1_smithg.py
@@ -0,0 +1,17 @@
+#CTI 110
+# M2T1 - Sales Prediction
+# 8/30
+# SmithG
+
+#Suggested variables : totalSales, annualProfit
+
+# ask the user for total sales
+totalSales = int(input("Type total sales: "))
+
+#calculate the profit amount
+annualProfit = 0.23 * totalSales
+
+#print the profit amount
+print(annualProfit)
+
+    
